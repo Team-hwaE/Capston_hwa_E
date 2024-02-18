94465fcf3a035ba422bf01ba36776a3e490f1bab
Dahee <d.hee@ewhain.net>
2024-02-19 01:24:17 +0900

    연동테스트

--- application.py
+++ application.py
@@ -3,11 +3,12 @@ from flask import redirect, url_for, session, jsonify
 import sys
 import pymysql
 import mysql.connector
-
+from database import DBhandler
 
 application = Flask(__name__)
 application.secret_key = 'your_secret_key'
 
+db = DBhandler()
 
 def get_initial_data():
     # 데이터베이스 연결
@@ -22,6 +23,8 @@ def get_initial_data():
 
     cursor.execute("SELECT * FROM user")
     user = cursor.fetchall()
+
+  
 
     # Database 닫기
     cursor.close()
@@ -93,9 +96,60 @@ def Select_category():
     return render_template('Select_category.html', current_page='Select_category.html')
 
 
+def update_user_ingredient():
+    email = session.get('user_email')  # 세션에서 이메일 주소 가져오기
+
+    if email:
+        db = DBhandler()
+
+        # 특정 이메일을 가진 사용자의 userID 검색
+        query = "SELECT userID FROM user WHERE email = %s"
+        user_id = db.execute_query(query, (email,))
+        
+        if user_id:
+            user_id = user_id[0][0]  # 튜플 형태로 반환되므로 첫 번째 요소만 사용
+            print(f"{user_id}")
+            # 해당 사용자가 구매한 productID를 사용하여 productIngredients 검색
+            query = "SELECT productIngredients FROM product WHERE productID IN (SELECT productID FROM user WHERE email = %s)"
+            product_ingredients = db.execute_query(query, (email,))
+
+            # productIngredients를 하나의 리스트로 합치기
+            ingredients_list = ', '.join([row[0] for row in product_ingredients])
+            print(f"합친 성분리스트 {ingredients_list}")
+            # 위에서 얻은 ingredientsList를 userIngredient 테이블에 저장
+            # 이미 동일한 userID가 존재하는지 확인하는 쿼리
+            query = "SELECT COUNT(*) FROM userIngredients WHERE userID = %s"
+            count = db.execute_query(query, (user_id,))[0][0]
+
+            if count > 0:
+                # 이미 동일한 userID가 존재하면 업데이트
+                query = "UPDATE userIngredients SET ingredientsList = %s WHERE userID = %s"
+                db.execute_query(query, (ingredients_list, user_id))
+            else:
+                # 존재하지 않으면 새로운 행 삽입
+                query = "INSERT INTO userIngredients (userID, ingredientsList) VALUES (%s, %s)"
+                db.execute_query(query, (user_id, ingredients_list))
+            db.commit()
+            db.close_connection()
+
+            return "User ingredient updated successfully."
+        else:
+            db.close_connection()
+            return "User not found."
+    else:
+        return "No email found in session."
+
+
 @application.route('/Recommend')
 def Recommend():
+    category = request.args.get('category','')
+    # user ingredient 리스트 만드는 함수 실행
+    update_user_ingredient()
+    # 머신러닝 함수 실행하기 (database나 백엔드단에서 작성)
+    # category 변수에 저장되어있음
+    print(f"선택한 카테고리 {category}")
     return render_template('Recommend.html', current_page='Recommend.html')
+
 
 
 @application.route('/Restart')
--- database.py
+++ database.py
@@ -29,3 +29,35 @@ def insert_item(self, name, data, img_path, sessionid):
         return True
 
 """
+
+import json
+import pymysql
+import mysql.connector
+
+class DBhandler:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host="127.0.0.1",
+            user="root",
+            password="0322",
+            database="skintreedb"
+        )
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        self.connection.commit()
+        return self.cursor.fetchall()
+    
+
+    def commit(self):
+        self.connection.commit()
+        
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
